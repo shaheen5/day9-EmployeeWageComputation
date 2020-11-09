44d9dcacf6a7c6803273a628db32713813eec516
unknown <miyashaheen1995@gmail.com>
2020-11-09 13:13:57 +0530

    Refactor:Changed if else to switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,16 +5,18 @@ echo "Welcome to Employee Wage Computation Program !"
 isPartTime=1;
 isFullTime=2;
 empRatePerHour=20;
-randomCheck=$((RANDOM%3));
+empCheck=$((RANDOM%3));
 
-if [ $isFullTime -eq $randomCheck ]
-then
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ]
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
+case $empCheck in
+	    $isFullTime)
+		    empHrs=8
+		    ;;
+	     $isPartTime)
+		    empHrs=4
+		    ;;
+	     *)
+	      empHrs=0
+		    ;;
+esac
 
 salary=$((empHrs*empRatePerHour));
