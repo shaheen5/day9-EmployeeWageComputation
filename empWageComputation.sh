adfb48d70731d03820073e59cc412a6881ea1822
unknown <miyashaheen1995@gmail.com>
2020-11-09 12:53:32 +0530

    Modified:Calculating daily wage according to emp attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,7 +6,9 @@ isPresent=1;
 randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ]
 then
-	echo "Employee is Present"
+	empRatePerHour=20;
+	empHrs=8;
+	salary=$((empHrs*empRatePerHour));
 else
-	echo "Employee is Absent"
+	salary=0;
 fi
