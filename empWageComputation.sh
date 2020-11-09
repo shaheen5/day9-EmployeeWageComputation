ca00aac8d641e159c4afa67ab5a2a47e15bb1665
unknown <miyashaheen1995@gmail.com>
2020-11-09 16:35:45 +0530

    Modified:Calculating wages till condition of total working hours or days is reached

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,14 +2,21 @@
 
 echo "Welcome to Employee Wage Computation Program !"
 
+#constants
 isPartTime=1;
 isFullTime=2;
-empRatePerHour=20;
-totalSalary=0;
-working_days_per_month=20;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HOUR=20;
+NUM_WORKING_DAYS=20;
 
-for (( day=1 ; day <= $working_days_per_month ; day++ ))
+#variables
+totalEmpHr=0;
+totalWorkingDays=0
+
+
+while [ $totalEmpHr -lt $MAX_HRS_IN_MONTH ] && [ $totalWorkingDays -lt $NUM_WORKING_DAYS ]
 do
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3));
 	case $empCheck in
 	    $isFullTime)
@@ -22,7 +29,6 @@ do
 	      empHrs=0
 		    ;;
 	esac
-
-	salary=$((empHrs*empRatePerHour));
-	totalSalary=$((totalSalary+salary));
+	totalEmpHr=$(($totalEmpHr+$empHrs))
 done
+totalSalary=$((totalEmpHr*$EMP_RATE_PER_HOUR))
