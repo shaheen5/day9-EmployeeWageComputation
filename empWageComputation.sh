fbcd0129ebf7e1bde2c2c7557d4e2c70ac1eb807
unknown <miyashaheen1995@gmail.com>
2020-11-09 17:27:55 +0530

    Refactor:added function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,22 +13,28 @@ NUM_WORKING_DAYS=20;
 totalEmpHr=0;
 totalWorkingDays=0
 
+#function to get employee work hours
+getEmployeeWorkHours(){
+
+case $1  in
+	 $isFullTime)
+                   empHrs=8
+                    ;;
+         $isPartTime)
+                    empHrs=4
+                    ;;
+          *)
+            empHrs=0
+                    ;;
+esac
+echo $empHrs;
+
+}
 
 while [ $totalEmpHr -lt $MAX_HRS_IN_MONTH ] && [ $totalWorkingDays -lt $NUM_WORKING_DAYS ]
 do
-	((totalWorkingDays++))
-	empCheck=$((RANDOM%3));
-	case $empCheck in
-	    $isFullTime)
-		    empHrs=8
-		    ;;
-	     $isPartTime)
-		    empHrs=4
-		    ;;
-	     *)
-	      empHrs=0
-		    ;;
-	esac
-	totalEmpHr=$(($totalEmpHr+$empHrs))
+	((totalWorkingDays++));
+	empHrs=$(getEmployeeWorkHours $((RANDOM%3)) );
+	totalEmpHr=$(($totalEmpHr+$empHrs));
 done
 totalSalary=$((totalEmpHr*$EMP_RATE_PER_HOUR))
