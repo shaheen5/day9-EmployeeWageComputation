8005c9879b52118d77a11e4a584252777842fa8b
unknown <miyashaheen1995@gmail.com>
2020-11-09 12:45:18 +0530

    Added: Check employee attendance using random function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,11 @@
 
 echo "Welcome to Employee Wage Computation Program !"
 
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
